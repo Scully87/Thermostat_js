ccca9b09cfff9a70af26ecd14b22f214dacc5b6d
Scully87 <scully87.ts@googlemail.com>
2014-10-22 10:31:51 +0100

    created first tests, defaults and custom options

---
+++ src/thermostat.js
@@ -0,0 +1,22 @@
+function Thermostat() {
+	this.temperature = 20;
+	this.isPowerSaverOn = true
+};
+
+Thermostat.prototype.increaseTemperature = function() {
+	this.increaseTemperatureBy(1)
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+	this.decreaseTemperatureBy(1);
+};
+
+Thermostat.prototype.increaseTemperatureBy = function(degrees) {
+	this.temperature += degrees
+};
+
+Thermostat.prototype.decreaseTemperatureBy = function(degrees) {
+	this.temperature -= degrees
+};
+
+
